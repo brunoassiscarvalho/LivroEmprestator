e28d3d085ab3ba4162986be9231e7069eadee873
brunoassiscarvalho <m3rcurio.br@gmail.com>
2017-09-17 00:01:18 -0300

    Alarm Manger com notification

--- app/src/main/java/br/com/m3rcurio/livroemprestator/AlarmReceiver.java
+++ app/src/main/java/br/com/m3rcurio/livroemprestator/AlarmReceiver.java
@@ -1,8 +1,13 @@
 package br.com.m3rcurio.livroemprestator;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import java.util.Date;
@@ -12,10 +17,30 @@ import java.util.Date;
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
+    String tituloLivro;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        tituloLivro = (String) intent.getSerializableExtra("tituloLivro");
         Log.i("ALARME", "O alarme executou as: "+new Date());
+        Resources res = context.getResources();
+        String nomeApp = res.getString(R.string.app_name);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_local_library_black_24dp)
+                        .setContentTitle(nomeApp)
+                        .setContentText("1o dias com: "+tituloLivro);
+        Intent resultIntent = new Intent(context, LoginActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(LoginActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent( 0, PendingIntent.FLAG_UPDATE_CURRENT );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
+
     }
 
 
